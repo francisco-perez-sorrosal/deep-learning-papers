4435ebc28419252b31b8018d6ae5cae144264056
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2020-09-04 16:17:23 -0700

    Add checkpoint after tool desc
    Also postBuild added for binder/RISE config

--- config_rise.py
+++ config_rise.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 from traitlets.config.manager import BaseJSONConfigManager
 from pathlib import Path
+
 path = Path.home() / ".jupyter" / "nbconfig"
 cm = BaseJSONConfigManager(config_dir=str(path))
 cm.update(
     "rise",
     {
-        "theme": "sky",
-        "transition": "zoom",
+        "theme": None,
+        "transition": None,
         "start_slideshow_at": "selected",
         "leap_motion": {
             "naturalSwipe"  : True,     # Invert swipe gestures
             "pointerOpacity": 0.5,      # Set pointer opacity to 0.5
             "pointerColor"  : "#d80000" # Red pointer"nat.png"
         },
-        "header": "<h1>Hello</h1>",
-        "footer": "<h3>World!</h3>"
+        "header": "<h3>Francisco Perez-Sorrosal</h3>",
+        "footer": "<h3>Deep Learning Reading Group</h3>",
+        "scroll": True,
+        "enable_chalkboard": True
      }
 )
+
+print("RISE config updated!!!")
